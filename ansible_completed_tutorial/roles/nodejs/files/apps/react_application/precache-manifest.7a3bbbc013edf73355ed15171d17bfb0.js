f0fb2a7c7f390f88e0a7ec7cf576c73eb571e7c0
Michael van Gastel <michael.van.gastel@amis.nl>
2019-02-12 12:53:48 +0100

    First commit: adds react application, completed tutorial, presentation, handson and starting point of tutorial

---
+++ ansible_completed_tutorial/roles/nodejs/files/apps/react_application/precache-manifest.7a3bbbc013edf73355ed15171d17bfb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b187ddfd2a6f3e2554a1",
+    "url": "/static/js/main.b187ddfd.chunk.js"
+  },
+  {
+    "revision": "8df897d9a7991c9f1b46",
+    "url": "/static/js/1.8df897d9.chunk.js"
+  },
+  {
+    "revision": "b187ddfd2a6f3e2554a1",
+    "url": "/static/css/main.867695b6.chunk.css"
+  },
+  {
+    "revision": "e07ad56eec3570843af1ed0be6a1d6ab",
+    "url": "/index.html"
+  }
+];
